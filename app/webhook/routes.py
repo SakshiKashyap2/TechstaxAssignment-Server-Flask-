5e4fcdaa86848eadb872426ec7f504a839271e6b
Sakshi <0909sakshikumari@gmail.com>
2024-04-03 16:36:52 +0530

    Test route is added

--- app/webhook/routes.py
+++ app/webhook/routes.py
@@ -2,6 +2,14 @@ from flask import Blueprint, json, request, jsonify
 
 webhook = Blueprint('Webhook', __name__, url_prefix='/webhook')
 
+
+
+
+@webhook.route('/test', methods=["GET"])
+def home():
+    return "Server Active!"
+
+
 @webhook.route('/receiver', methods=["POST"])
 def receiver():
 
